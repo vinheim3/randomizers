263eee3b393d340350640c1d684055221082e01b
Daniel Jianoran <dancjia@gmail.com>
2022-08-07 08:19:16 +0100

    MMX3: enemy damage/health multiplier

--- src/js/mmx3.js
+++ src/js/mmx3.js
@@ -50,6 +50,155 @@ function randomize(_rom, rng, opts) {
         rtl
     `);
 
+    if (opts.enemy_multipliers !== '1') {
+        let mult = parseFloat(opts.enemy_multipliers);
+
+        // start with enemy table
+        start = conv(6, 0xe28e);        
+        for (let enemyId = 0; enemyId <= 0x6a; enemyId++) {
+            if (enemyId != 9) {
+                if (rom[start+2]!==0) {
+                    let newDmgDealt = Math.max(1, Math.floor(rom[start+2]*mult));
+                    rom[start+2] = newDmgDealt;
+                }
+                if (rom[start+3]!==0) {
+                    let newHealth = Math.max(1, rom[start+3]*mult);
+                    rom[start+3] = newHealth;
+                }
+            }
+            start += 5;
+        }
+
+        // then boss health
+        for (let [bossName, deets] of Object.entries(bossData)) {
+            let healthAddr = deets.maxHealth;
+            if (rom[healthAddr-1] !== 0xc9 || rom[healthAddr] !== 0x20)
+                throw new Error(`Boss ${bossName} health address is wrong`);
+
+            let health = Math.floor(rom[healthAddr]*mult);
+            rom[healthAddr] = health;
+            bossData[bossName].newHealth = health;
+        }
+
+        // ride armour goliath cross-charge table
+        start = conv(6, 0xf353);
+        for (let i = 0; i < 4; i++) {
+            let dmg = Math.max(1, Math.floor(rom[start+i]*mult));
+            rom[start+i] = dmg;
+        }
+
+        // then misc `lda/adc #xx.b`
+        for (let addr of [
+            conv(0x3, 0xcb21),
+            conv(0x3, 0xcb47),
+            conv(0x3, 0xcbb0),
+            conv(0x3, 0xcc9f),
+            conv(0x3, 0xd0f5),
+            conv(0x3, 0xd31e),
+            conv(0x3, 0xd39d),
+            conv(0x4, 0xc8dd),
+            conv(0x4, 0xe407),
+            conv(0x4, 0xe441),
+            conv(0x4, 0xe48e),
+            conv(0x4, 0xe501),
+            conv(0x4, 0xe553),
+            conv(0x4, 0xe578),
+            conv(0x4, 0xeabd),
+            conv(0x4, 0xec22),
+            conv(0x4, 0xeda3),
+            conv(0x4, 0xef17),
+            conv(0x4, 0xf058),
+            conv(0x4, 0xf26a),
+            conv(0x4, 0xf643),
+            conv(0x4, 0xf911),
+            conv(0x4, 0xfb81),
+            conv(0x5, 0x8ba4),
+            conv(0x5, 0x8e9c),
+            conv(0x5, 0x99c6),
+            conv(0x5, 0xb230),
+            conv(0x5, 0xb267),
+            conv(0x5, 0xb39c),
+            conv(0x5, 0xb3d3),
+            conv(0x5, 0xb561),
+            conv(0x5, 0xb5a5),
+            conv(0x5, 0xca3b),
+            conv(0x7, 0x859e),
+            conv(0x7, 0x8e78),
+            conv(0x7, 0xc48a),
+            conv(0x7, 0xf73d),
+            conv(0x7, 0xfe75),
+            conv(0x8, 0x9a50),
+            conv(0x8, 0x9a90),
+            conv(0x8, 0x9b83),
+            conv(0x8, 0x9d32),
+            conv(0x8, 0x9d67),
+            conv(0x8, 0xa16f),
+            conv(0x8, 0xa1c8),
+            conv(0x8, 0xa61b),
+            conv(0x8, 0xa635),
+            conv(0x8, 0xa657),
+            conv(0x8, 0xaa07),
+            conv(0x8, 0xaad9),
+            conv(0x8, 0xab0f),
+            conv(0x8, 0xaf04),
+            conv(0x8, 0xaf3a),
+            conv(0x8, 0xaf7d),
+            conv(0x8, 0xaf91),
+            conv(0x8, 0xb62f),
+            conv(0x8, 0xb6e5),
+            conv(0x8, 0xbcdc),
+            conv(0x8, 0xbe9b),
+            conv(0x8, 0xbed3),
+            conv(0x8, 0xc098),
+            conv(0x8, 0xc0c0),
+            conv(0x8, 0xc775),
+            conv(0x8, 0xca03),
+            conv(0x13, 0xd2e5),
+            conv(0x13, 0xd2f6),
+            conv(0x13, 0xd30d),
+            conv(0x13, 0xd44b),
+            conv(0x13, 0xdd4e),
+            conv(0x13, 0xe53f),
+            conv(0x13, 0xe7bc),
+            conv(0x13, 0xe898),
+            conv(0x13, 0xeaff),
+            conv(0x13, 0xf01c),
+            conv(0x13, 0xf040),
+            conv(0x13, 0xf2f2),
+            conv(0x13, 0xf5e3),
+            conv(0x32, 0xf313),
+            conv(0x32, 0xf331),
+            conv(0x32, 0xf371),
+            conv(0x39, 0x9cd5),
+            conv(0x39, 0x9ef6),
+            conv(0x39, 0x9f0f),
+            conv(0x39, 0xa590),
+            conv(0x39, 0xa5bf),
+            conv(0x39, 0xa5f0),
+            conv(0x39, 0xac93),
+            conv(0x39, 0xacf9),
+            conv(0x39, 0xae86),
+            conv(0x39, 0xaf4a),
+            conv(0x39, 0xb0d5),
+            conv(0x39, 0xb394),
+            conv(0x3a, 0xf998),
+            conv(0x3c, 0xbf59),
+            conv(0x3d, 0xfd60),
+            conv(0x3d, 0xfda9),
+            conv(0x3f, 0xe6a7),
+            conv(0x3f, 0xe957),
+            conv(0x3f, 0xe9e6),
+            conv(0x3f, 0xeadd),
+            conv(0x3f, 0xeff6),
+        ]) {
+            if (rom[addr] === 0xea) continue;
+            if (rom[addr] !== 0xa9 && rom[addr] !== 0x69)
+                throw new Error(`Enemy misc dmg invalid ${hexc(addr)}`);
+            let newDmg = Math.max(1, Math.floor(rom[addr+1]*mult));
+            rom[addr+1] = newDmg;
+        }
+    }
+
     // Scavenger hunt: num subweapons required
     if (opts.new_game_mode === 'doppler_subwep_locked' || opts.new_game_mode === 'doppler_upgrades_locked') {
         let num_required = opts.new_game_mode === 'doppler_subwep_locked' ? opts.subweps_required : opts.upgrades_required;
@@ -121,19 +270,20 @@ function randomize(_rom, rng, opts) {
 
                     pla
                     cmp #$0${num_required}.b
-                    bcs _sufficientSubweapons1
+                    bcs _sufficientSubweapons
 
                     plp
                     lda #$00.b
                     rtl
 
-                _sufficientSubweapons1:
+                _sufficientSubweapons:
                     plp
                     lda #$ff.b
                     rtl
                 `);
             } else {
                 m.addAsm(null, null, `
+                ZeroModCheckGotSufficientSubweapons:
                     php
                     sep #$30.b
                     lda wSubTanksAndUpgradesGottenBitfield.w
@@ -162,7 +312,7 @@ function randomize(_rom, rng, opts) {
                 ; End of copy
 
                     cmp #$0${num_required}.b
-                    bcs _sufficientSubweapons2
+                    bcs _sufficientSubweapons
 
                     pla
                     sta wSubTanksAndUpgradesGottenBitfield.w
@@ -170,7 +320,7 @@ function randomize(_rom, rng, opts) {
                     lda #$00.b
                     rtl
 
-                sufficientSubweapons1:
+                _sufficientSubweapons:
                     pla
                     sta wSubTanksAndUpgradesGottenBitfield.w
                     plp
@@ -449,8 +599,21 @@ function randomize(_rom, rng, opts) {
             nop
             nop
         `);
-        m.addAsm(4, 0xd095, `
-            sbc #$00.b
+        m.addAsm(4, 0xc8dd, `
+            lda #$00.b
+        `);
+        // earth commander roller
+        m.addAsm(7, 0x9c54, `
+            nop
+            nop
+        `);
+        // caterkiller charged
+        m.addAsm(4, 0xedc8, `
+            nop
+            nop
+        `);
+        // kaiser sigma light
+        m.addAsm(5, 0x8da1, `
             nop
         `);
     }
